e07bab2b1b0cb3db9e23ac03bccdfcd04b8eef16
Dizzrt <dizzrt.s@qq.com>
2026-01-20 03:04:56 +0800

    use go dns resolver

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/dizzrt/dauth/cmd"
+import (
+	"os"
+
+	"github.com/dizzrt/dauth/cmd"
+)
+
+func init() {
+	// use go dns resolver
+	os.Setenv("GODEBUG", "netdns=go")
+}
 
 func main() {
 	cmd.Execute()
